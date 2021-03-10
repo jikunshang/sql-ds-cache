ca02658fe850b2fa3f2f4acf6c14e18c7f76f2b6
Kunshang Ji <kunshang.ji@intel.com>
2021-03-10 14:39:40 +0800

    add sudo

--- ci_scripts/install_APE_dependency.sh
+++ ci_scripts/install_APE_dependency.sh
@@ -9,13 +9,13 @@ mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
 make -j
-make install
+sudo make install
 
 # install hi-redis, redis-plus-plus
 cd /tmp
 git clone https://github.com/redis/hiredis.git
 cd hiredis
-make USE_SSL=1 PREFIX=$INSTALL_DIR install -j
+sudo make USE_SSL=1 PREFIX=$INSTALL_DIR install -j
 
 cd /tmp
 git clone https://github.com/sewenew/redis-plus-plus.git
@@ -24,7 +24,7 @@ mkdir compile
 cd compile
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR ..
 make -j
-make install
+sudo make install
 
 
 # install openssl-dev
